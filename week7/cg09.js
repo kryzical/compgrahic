e50856f4c1041d90cb302fae7b50bb403a48bc01
brian <kryptozinc@gmail.com>
2025-10-07 09:14:16 -0500

    push

---
+++ week7/cg09.js
@@ -0,0 +1,108 @@
+"use strict"
+
+// vertex shader code (vsc)
+const vsc = "attribute vec2 vpos;" +
+"void main() {" +
+"gl_Position = vec4(vpos, 0.0, 1.0);" +
+"}";
+
+// fragment shader code (fsc)
+const fsc = "precision lowp float;" + 
+"uniform vec4 vcolor;" + 
+"void main() {" +
+"gl_FragColor = vcolor;" +
+"}";
+
+const house = [
+    [0,.4,1, 0,0,1, .5,0,1, .5,.4,1, .25,.65,1, 0,.4,1, .5,.4,1], // roof and body
+    [0.1,0,1, .1,.15,1, .15,.15,1, .15,0,1], // door
+    [.35,.25,1, .35,.35,1, .45,.35,1, .45,.25,1, .35,.25,1], // window frame
+    [.35,.3,1, .45,.3,1], // window cross x
+    [.4,.25,1, .4,.35,1], // window cross y
+    [.25+.25/2,.5+.025,1, .25+.25/2,.5+.25/2,1, .25+.175,.5+.25/2,1,
+    .25+.175,.4+.075,1] // chimney
+];
+
+const xy_axis = [
+    [-1,0,1, 1,0,1], // x
+    [0,1,1, 0,-1,1]  // y  
+];
+
+
+
+let gl;
+let gl_prog;
+let canvas;
+const N_DIM = 3;  // 2d homogenous coord
+let unif_vcolor;
+
+function create_gl_program() {
+    let vs = gl.createShader(gl.VERTEX_SHADER);
+    let fs = gl.createShader(gl.FRAGMENT_SHADER);
+    gl.shaderSource(vs, vsc);
+    gl.shaderSource(fs, fsc);
+    gl.compileShader(vs);
+    gl.compileShader(fs);
+    gl_prog = gl.createProgram();
+    gl.attachShader(gl_prog, vs);
+    gl.attachShader(gl_prog, fs);
+    gl.linkProgram(gl_prog);
+}
+
+function init_gl() {
+    canvas = document.getElementById("webgl_canvas");
+    gl = canvas.getContext("webgl");
+    create_gl_program();
+    gl.useProgram(gl_prog);
+    
+    gl.clearColor(0,0,0,1);
+    gl.clear(gl.COLOR_BUFFER_BIT);    
+
+    gl.bindBuffer(gl.ARRAY_BUFFER, gl.createBuffer());
+    let attr_vpos = gl.getAttribLocation(gl_prog, "vpos");
+    gl.vertexAttribPointer(attr_vpos, N_DIM, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(attr_vpos);
+
+    unif_vcolor = gl.getUniformLocation(gl_prog, "vcolor");
+}
+
+function transform(input) {
+    switch(input.id) {
+        case "scale" : 
+            console.log(input.value);
+            break;
+        case "rotate" :
+            console.log(input.value);
+            break;
+    }
+    
+}
+
+function draw_line(vertices) {
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+    gl.drawArrays(gl.LINES, 0, 3);
+}
+
+function draw_house(vertices,color) {
+    for (let i=0; i<vertices.length; ++i) {
+        gl.uniform4f(unif_vcolor, color[0], color[1], color[2], 1);
+        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices[i]), gl.STATIC_DRAW);
+                                    // process the vertices in N_DIM pieces
+        gl.drawArrays(gl.LINE_STRIP, 0, vertices[i].length/N_DIM);
+    }
+}
+
+function draw_xy_axis() {
+    draw_line(xy_axis[0]);
+    draw_line(xy_axis[1]);
+}
+
+function main() {
+    let magenta_color = [1,0,1];
+    init_gl();
+    draw_xy_axis();
+    draw_house(house,magenta_color);
+
+}
+
+main();
---
+++ week7/util.js
@@ -0,0 +1,62 @@
+function randomf(min,max) {
+    return Math.random() * (max - min) + min;
+}
+
+function deg_to_rad(deg) {
+    return deg * (Math.PI / 180);
+}
+
+function get_scale_3d(Sx,Sy) {
+  return [
+    [Sx, 0, 0],
+    [0, Sy, 0],
+    [0, 0, 1]
+  ];
+}
+
+function get_translate_3d(Tx,Ty) {
+  return [
+    [1, 0, Tx],
+    [0, 1, Ty],
+    [0, 0, 1]
+  ];
+}
+
+function rotate_3d(theta) {
+   return [
+      [ Math.cos(theta),-Math.sin(theta), 0],
+      [ Math.sin(theta), Math.cos(theta), 0],
+      [     0 ,           0,        1]
+   ]
+}
+
+function mat_mat_mul_3d(M1,M2) {
+    return [
+      [
+        M1[0][0]*M2[0][0] + M1[0][1]*M2[1][0] + M1[0][2]*M2[2][0],
+        M1[0][0]*M2[0][1] + M1[0][1]*M2[1][1] + M1[0][2]*M2[2][1],
+        M1[0][0]*M2[0][2] + M1[0][1]*M2[1][2] + M1[0][2]*M2[2][2]
+      ],
+
+      [
+        M1[1][0]*M2[0][0] + M1[1][1]*M2[1][0] + M1[1][2]*M2[2][0],
+        M1[1][0]*M2[0][1] + M1[1][1]*M2[1][1] + M1[1][2]*M2[2][1],
+        M1[1][0]*M2[0][2] + M1[1][1]*M2[1][2] + M1[1][2]*M2[2][2]
+      ],
+
+      [
+        M1[2][0]*M2[0][0] + M1[2][1]*M2[1][0] + M1[2][2]*M2[2][0],
+        M1[2][0]*M2[0][1] + M1[2][1]*M2[1][1] + M1[2][2]*M2[2][1],
+        M1[2][0]*M2[0][2] + M1[2][1]*M2[1][2] + M1[2][2]*M2[2][2]
+      ]
+    ];
+}
+
+// scaling 
+function mat_vec_mul_3d(M,v) {
+  return [
+    M[0][0]*v[0] + M[0][1]*v[1] + M[0][2]*v[2],
+    M[1][0]*v[0] + M[1][1]*v[1] + M[1][2]*v[2],
+    M[2][0]*v[0] + M[2][1]*v[1] + M[2][2]*v[2],
+  ];
+}
